784995a46669576c429f39eed43bb7efe8151c23
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2021-01-23 22:47:38 -0300

    Desafio finalizado

--- scripts/script.js
+++ scripts/script.js
@@ -2,93 +2,82 @@
 let requestURL = 'https://quiz-trainee.herokuapp.com/questions'; // Link para perguntas/respostas
 let request = new XMLHttpRequest(); // Instanciando um novo objeto XHR
 let signal = false; // Sinalizador
-let sigLis = false;
+let sigLis = false; // Sinalizado btn
 let numQ; // Num de questões
 let answer; // Resposta
-let indQ = 0; // Indice
+let indQ = 0; // Indice questão
 let sumPont; // Maior pontuação possivel
 let sumJog = 0; // Pontuação do jogador
 let titulo = document.getElementById('titulo'); // Titulo
 let btnConfirmar = document.getElementById('confirmar'); // Botão Next
-let divResultado = document.getElementById('resultado'); // Resultado
 let listaRespostas = document.getElementById('listaRespostas'); // Lista de respostas
-let resultFinal = document.getElementById('resultado');
-
+let resultFinal = document.getElementById('resultado'); // Resultado
 
 // Funções
-request.onload = function(){ // Aguardando a Resposta
-    answer = request.response;
-    numQ = answer.length;
+request.onload = function () { // Aguardando a Resposta
+    answer = request.response; // Atribuindo a answer, a resposta dada pelo servidor
+    numQ = answer.length; // Número de questões
     sumPont = 3 * numQ;
 }
 
-// Ouvindo eventos que ocorrem com a listaRespostas.
-function radioSelect(){
+function radioSelect () { // Ouvindo eventos que ocorrem com a listaRespostas.
     sigLis = true;
-    console.log("Para de clicar, pfv!");
 }
 
-for (let i = 0 ; i < 4 ; i++){
-    listaRespostas.children[i].children[0].onclick = radioSelect;
-}
-
-function limparSelecao(){ // Limpando seleção dos inputs
+function limparSelecao () { // Limpando seleção dos inputs
     sigLis = false;
-    for (let i = 0 ; i < 4 ; i++){
-        if ((listaRespostas.children[i].children[0].checked) === true){
+    for ( let i = 0 ; i < 4 ; i++ ) {
+        if ( (listaRespostas.children[i].children[0].checked) === true ) {
             listaRespostas.children[i].children[0].checked = false;
-            sumJog = (sumJog + answer[indQ - 1].options[i].value);
-            console.log(sumJog);
+            sumJog = ( sumJog + answer[indQ - 1].options[i].value ); // Recebendo o valor da opção escolhida pelo jogador
         } else {
             listaRespostas.children[i].children[0].checked = false;
         }
     }
 }
 
-function escreverQuestao(){ // escreverQuestao
-    titulo.textContent = answer[indQ].title;
-    for (let i = 0 ; i < 4 ; i++){
+function escreverQuestao () { // escreverQuestao
+    titulo.textContent = answer[indQ].title; // Alterando o titulo
+    for ( let i = 0 ; i < 4 ; i++ ) {
+        // Alterando os itens das questões
         listaRespostas.children[i].children[1].textContent = answer[indQ].options[i].answer;
     }
-    console.log("Questão escrita com sucesso");
 }
 
-function finalizarQuiz(){ // finalizarQuiz
-    let perPont = 0;
+function finalizarQuiz () { // finalizarQuiz
+    let perPont = 0; // Porcentagem apresentada no final do quiz
     sigLis = true;
-    titulo.textContent = "QUIZ DOS VALORES DA GTI";
-    listaRespostas.style.display = "none";
-    perPont = ((sumJog / sumPont) * 100);
-    resultFinal.textContent = "Sua pontuação: " + perPont + "%";
+    titulo.textContent = "QUIZ DOS VALORES DA GTI"; // Titulo inicial/final
+    listaRespostas.style.display = "none"; // Escondendo listaRespostas
+    perPont = ( (sumJog / sumPont) * 100 ); // Calculo da porcetagem de acertos do jogador
+    resultFinal.textContent = "Sua pontuação: " + perPont + "%"; // Apresentando a pontuação do jogador
     btnConfirmar.textContent = "Refazer quiz";
-    console.log("Quiz finalizado");
 }
 
-function reiniciarQuiz(){ // reiniciarQuiz
-    console.log("Quiz reiniciado");
-    listaRespostas.style.display = "block";
-    resultFinal.textContent = "";
+function reiniciarQuiz () { // reiniciarQuiz
+    listaRespostas.style.display = "block"; 
+    resultFinal.textContent = ""; // Limpando o resultado
     btnConfirmar.textContent = "PROXIMA";
     indQ = 0;
-    sumJog = 0;
+    sumJog = 0; // Zerando a pontuação do jogador
     escreverQuestao();
     indQ++;
 }
 
-function mostrarQuestao(){ // mostrarQuestao
-    if (!signal){
+function mostrarQuestao () { // mostrarQuestao
+    if ( !signal ) {
         signal = true;
         escreverQuestao();
-        listaRespostas.style.display = "block";
-        btnConfirmar.textContent = "PROXIMA";
+        listaRespostas.style.display = "block"; // Exibindo listaRespostas
+        btnConfirmar.textContent = "PROXIMA"; // Alterando o texto do botão
         indQ++;
-    } else if (signal && sigLis) {
+    } else if ( signal && sigLis ) {
         limparSelecao();
-        if (indQ <= (numQ - 1)){
+        if ( indQ <= (numQ - 1) ) {
             escreverQuestao();
             indQ++;
         } else {
-            if (indQ === numQ){
+            if ( indQ === numQ ) {
                 finalizarQuiz();
                 indQ++;
             } else {
@@ -98,12 +87,10 @@ function mostrarQuestao(){ // mostrarQuestao
     }
 }
 
+for ( let i = 0 ; i < 4 ; i++ ) { // Quando input for selecionado, a função radioSelect será chamada
+    listaRespostas.children[i].children[0].onclick = radioSelect;
+}
 
-// "Main"
-
-// Abrindo uma solicitação
-request.open('GET', requestURL, true);
-// Informando o tipo da estrutura de dados
-request.responseType = 'json';
-// Enviando o pedido 
-request.send();
+request.open('GET', requestURL, true); // Abrindo uma solicitação
+request.responseType = 'json'; // Informando o tipo da estrutura de dados
+request.send(); // Enviando o pedido 
